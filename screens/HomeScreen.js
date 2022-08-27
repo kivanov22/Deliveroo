7aa52fae87c515df958c6131deedc43410b5d9eb
Kristian Ivanov <kriskopk@abv.bg>
2022-08-27 21:45:58 +0300

    created a scrollable body and components Categories,CategoryCard

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,20 @@
-import { SafeAreaView,View, Text, Image, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
   UserIcon,
   ChevronDownIcon,
-  SearchIcon,
-  AdjustmentsIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
+import Categories from "../components/Categories.js";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -19,9 +27,8 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text className="text-red-500">
         {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
           <Image
             source={{
               uri: "https://links.papareact.com/wru",
@@ -43,18 +50,30 @@ const HomeScreen = () => {
         </View>
 
         {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 ">
           <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
-            <SearchIcon color="gray" size={20}/>
-            <TextInput 
-            placeholder="Restaurants and cuisines"
-            keyboardType="default"
+            <MagnifyingGlassIcon color="gray" size={20} />
+            <TextInput
+              placeholder="Restaurants and cuisines"
+              keyboardType="default"
             />
           </View>
 
-          <AdjustmentsIcon color="#00CCBB"/>
+          <AdjustmentsVerticalIcon color="#00CCBB" />
         </View>
-      </Text>
+
+        {/* Body */}
+        <ScrollView 
+        className="bg-gray-100 "
+        contentContainerStyle={{
+          paddingBottom:100,
+        }}
+        >
+          {/* Categories */}
+            <Categories />
+
+          {/* Featured rows */}
+        </ScrollView>
     </SafeAreaView>
   );
 };
